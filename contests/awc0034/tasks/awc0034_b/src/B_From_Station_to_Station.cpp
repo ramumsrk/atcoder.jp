c01d0fb805721bca17728fd405926b6dc30e1705
Ramu Mohan Srinivasan RadhaKrishnan <ramumsrk02071981@gmail.com>
2026-03-26 17:35:00 +0530

    B_From_Station_to_Station.cpp: B - From Station to Station: https://atcoder.jp/contests/awc0034/tasks/awc0034_b On branch main_atcoder_jp Your branch is up to date with 'origin_atcoder_jp/main_atcoder_jp'. Changes to be committed: modified:   contests/awc0034/tasks/awc0034_b/src/B_From_Station_to_Station.cpp

--- contests/awc0034/tasks/awc0034_b/src/B_From_Station_to_Station.cpp
+++ contests/awc0034/tasks/awc0034_b/src/B_From_Station_to_Station.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
+using std::vector;
 using std::cin;
 using std::cout;
 using std::endl;
 
 auto
 main(void) -> int {
+    int N {};
+    cin>>N;
+    vector<int> P(N-1,0);
+    for (auto& p : P) {
+        cin>>p;
+    }
+    int stations {};
+    for (int i {}; i <= N-1; i += 1) {
+        if (P[i] == N) {
+            stations += 1;
+            break;
+        } else {
+            stations += 1;
+        }
+    }
+    cout<<((stations-1)+2)<<endl;
     return EXIT_SUCCESS;
 }
