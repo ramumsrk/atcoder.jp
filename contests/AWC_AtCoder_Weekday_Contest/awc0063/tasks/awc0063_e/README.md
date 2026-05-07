340d1f8cbe110ef018ac38fe743c1935fee5630b
Ramu Mohan Srinivasan RadhaKrishnan <ramumsrk02071981@gmail.com>
2026-05-07 10:49:49 +0530

    second attempt: E - Number of Blocks in an Interval: https://atcoder.jp/contests/awc0063/tasks/awc0063_e On branch main_atcoder_jp Your branch is up to date with 'origin_atcoder_jp/main_atcoder_jp'. Changes to be committed: modified:   contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/README.md modified:   contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/src/E_Number_of_Blocks_in_an_Interval.cxx

--- contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/README.md
+++ contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/README.md
@@ -1,1 +1,4 @@
 # [E - Number of Blocks in an Interval](https://atcoder.jp/contests/awc0063/tasks/awc0063_e)
+## Submissions
+### WA - Wrong Answer
+1. [75565044](https://atcoder.jp/contests/awc0063/submissions/75565044)
--- contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/src/E_Number_of_Blocks_in_an_Interval.cxx
+++ contests/AWC_AtCoder_Weekday_Contest/awc0063/tasks/awc0063_e/src/E_Number_of_Blocks_in_an_Interval.cxx
@@ -1,11 +1,43 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::vector;
 
 auto
 main(void) -> int {
+    int N {}, Q {};
+    cin>>N>>Q;
+    vector<long long int> C(N,0);
+    for (auto& c : C) {
+        cin>>c;
+    }
+    for (int q {}; q < Q; q += 1) {
+        int T {};
+        cin>>T;
+        int L {}, R {};
+        if (T == 1) {
+            int X {};
+            cin>>L>>R>>X;
+            for (int l {L-1}; l < R; l += 1) {
+                C[l] = X;
+            }
+        } else if (T == 2) {
+            cin>>L>>R;
+            int groups_count {};
+            for (int l {L-1}, r {l+1}; l < R; l += 1) {
+                while (r < R-1 && C[l] == C[r]) {
+                    r += 1;
+                }
+                l = r;
+                r = l+1;
+                groups_count += 1;
+            }
+            cout<<groups_count<<endl;
+        }
+    }
     return EXIT_SUCCESS;
 }
